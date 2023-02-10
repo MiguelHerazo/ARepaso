2cff1ab4b942f7b78be7c856662fe289227a961a
MiguelGerazo <herazomigue56@gmail.com>
2023-02-09 22:24:27 -0500

    Implementado Ejercicio7

---
+++ Ejercicio 7.py
@@ -0,0 +1,8 @@
+def mayormenor(lista):
+    mayor = max(lista)
+    menor = min(lista)
+
+    print(f"El numero mayor es {mayor} y el numero menor es {menor}")
+
+lista = [1,2,3,4,5]
+mayormenor(lista)
